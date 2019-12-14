08747bf55ebb139be49f08c559b51766506d0f02
Breakermoob <breakermoob@hotmail.com>
2019-12-14 09:24:38 -0500

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+//Expresss instance
 const app = express();
 
 // Serve only the static files form the dist directory
